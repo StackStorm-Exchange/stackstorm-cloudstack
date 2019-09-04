b069389695d4e17a1396a4e3865b7f553b5d136a
Igor Fernandes <igor.fernandes@corp.globo.com>
2019-09-04 13:26:37 -0300

    Fix style code

--- actions/force_destroy.py
+++ actions/force_destroy.py
@@ -7,6 +7,7 @@ __all__ = [
     'ForceStopVM'
 ]
 
+
 class ForceStopVM(Action):
     def __init__(self, config):
         super(ForceStopVM, self).__init__(config=config)
@@ -14,16 +15,16 @@ class ForceStopVM(Action):
     def run(self, url, apikey, secretkey, vm_id):
         url_temp = self.config.get('url')
 
-        if (url != "" and url != None):
+        if url != "" and url is not None:
             url_temp = url
 
         cs = CloudStack(endpoint=url_temp,
-                key=apikey,
-                secret=secretkey,
-                dangerous_no_tls_verify=True,
-                fetch_result=True)
+                        key=apikey,
+                        secret=secretkey,
+                        dangerous_no_tls_verify=True,
+                        fetch_result=True)
 
-        if cs.stopVirtualMachine(id = vm_id, forced = True):
-            return cs.destroyVirtualMachine(id = vm_id)
+        if cs.stopVirtualMachine(id=vm_id, forced=True):
+            return cs.destroyVirtualMachine(id=vm_id)
 
         return False
