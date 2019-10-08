ebc35477c1a0b8aec30331af5c2c82a27057a6e4
Felipe Oliveira <felipe.oliveira@mac221023.local>
2019-10-08 18:40:59 -0300

    Change duplicate names

--- actions/lb_attach_member.py
+++ actions/lb_attach_member.py
@@ -8,7 +8,7 @@ __all__ = [
 
 
 class LBAttachhMember(CloudStackAPI):
-    def run(self, url, apikey, secretkey, lb_id, virtualmachineids, virtualmachineids):
+    def run(self, url, apikey, secretkey, lb_id, vm_ids):
         cs = self.get_client(url, apikey, secretkey)
 
-        return cs.assignToLoadBalancerRule(id=lb_id, virtualmachineids=virtualmachineids)
+        return cs.assignToLoadBalancerRule(id=lb_id, virtualmachineids=vm_ids)
--- actions/lb_dettach_member.py
+++ actions/lb_dettach_member.py
@@ -8,7 +8,7 @@ __all__ = [
 
 
 class LBDettachhMember(CloudStackAPI):
-    def run(self, url, apikey, secretkey, lb_id, virtualmachineids):
+    def run(self, url, apikey, secretkey, lb_id, vm_ids):
         cs = self.get_client(url, apikey, secretkey)
 
-        return cs.removeFromLoadBalancerRule(id=lb_id, virtualmachineids=virtualmachineids)
+        return cs.removeFromLoadBalancerRule(id=lb_id, virtualmachineids=vm_ids)
